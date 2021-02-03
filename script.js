bff696ceec18c2e3cf6c43612e1ca2b44c682f12
Tetiana <brezhynska9012@gmail.com>
2021-02-03 21:03:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+// "use strict" everywhere for new fixed version of JS
+
+// alert("Hello!");
+// const result = confirm("Are you here?")
+// console.log(result);
+
+// const answer = prompt("Are you an adult?", "18");
+// console.log(answer + 5);
+
+// const answer = +prompt("Are you an adult?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt("What is your name?");
+answers[1] = prompt("What is your lastname?");
+answers[2] = prompt("How old are you?");
+
+document.write(answers);
