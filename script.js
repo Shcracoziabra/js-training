55da3c8841427faf58428378d0f93181c0471a40
Tetiana <brezhynska9012@gmail.com>
2021-02-04 19:47:54 +0200

    changed

--- script.js
+++ script.js
@@ -17,4 +17,4 @@ answers[0] = prompt("What is your name?");
 answers[1] = prompt("What is your lastname?");
 answers[2] = prompt("How old are you?");
 
-document.write(answers);
+console.log(answers);
