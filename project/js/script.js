33dcad38ad5af57dd77c53d9d44bb5d1c211db6f
Tetiana <brezhynska9012@gmail.com>
2021-02-21 16:14:25 +0200

    finished pactice in lecture 33

--- project/js/script.js
+++ project/js/script.js
@@ -29,58 +29,96 @@ document.addEventListener('DOMContentLoaded', () => {
     const addsDiv = document.getElementsByClassName('promo__adv')[0];
     const adds = addsDiv.querySelectorAll('img');
     
-    adds.forEach(item => {
-        item.remove();
-    });
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+
+    deleteAdv(adds);
     
-    // 2) Switch 'comedy' genre to 'drama'
-    const myPoster = document.querySelector('.promo__bg');
-    const myGenre = myPoster.querySelector('.promo__genre').textContent = 'драма';
-    console.log(myGenre);
+    // 2, 3) Switch 'comedy' genre to 'drama', change poster
+    const poster = document.querySelector('.promo__bg');
+    const genre = poster.querySelector('.promo__genre');
     
-    // ---------3) Change poster background
+    const makeChanges = () => {
+        genre.textContent = 'драма';
+        poster.style.backgroundImage = "url('img/bg.jpg')";
+    };
+
+    makeChanges();
     
-    myPoster.style.backgroundImage = "url('img/bg.jpg')";
     
     // 4) create a list of movies on the web page, do alphabet sorting
+
+    const sortArrOfStr = (arr) => {
+        arr.forEach(item => {
+            item = item.toUpperCase();
+        });
+        arr.sort();
+    };
     
-   
-    
-    const myMovieList = document.querySelectorAll('.promo__interactive-item');
-    const myListContent = [];
-    for(let i=0; i < movieDB.movies.length; i++){
-        myListContent[i] = movieDB.movies[i].toUpperCase();
-    } 
-    myListContent.sort();
-    console.log(myListContent);
-    
-    
-    for(let i=0; i < myListContent.length; i++){
-        myMovieList[i].textContent = myListContent[i];
-    } 
+
+
     
     const myMovieContainer =  document.querySelector('.promo__interactive-list');
-    myMovieContainer.innerHTML = '';
     
-    myListContent.forEach((film, i) =>{
-        myMovieContainer.innerHTML += `<li class="promo__interactive-item">${i + 1} ${film}
-            <div class = 'delete'></div>
-        </li>`;
-    });    
-    const btn = document.querySelector('button'),
-    newFilm = document.querySelector('.adding__input');
+    // function makes page movie list from sorted movieDB.movies array
 
+    function addSortedMovies(movies, parent) {
+
+        parent.innerHTML = '';
+        sortArrOfStr(movies);
+        movies.forEach((movie, i) =>{
+            myMovieContainer.innerHTML += `<li class="promo__interactive-item">${i + 1} ${movie}
+                <div class = 'delete'></div>
+            </li>`;
+        });
+
+        // delete icon delets movie from the page and movieDB.movies array
+
+        document.querySelectorAll('.delete').forEach((deleteIcon, i) => {
+            deleteIcon.addEventListener('click', () => {
+                deleteIcon.parentNode.remove();
+                movieDB.movies.splice(i, 1);
+                addSortedMovies(movies, parent); 
+            });
+        });
+
+    }
+
+    
+
+    addSortedMovies(movieDB.movies, myMovieContainer);
+
+      
+    const btn = document.querySelector('button'),
+    addInput = document.querySelector('.adding__input'),
+    checkbox = document.querySelector('.add input[type=checkbox]');
+
+// Make interactive form, that adds your movies to the page and movie array
+    
     btn.addEventListener('click', (event) =>{
         event.preventDefault();
-        if(newFilm.value != '') {
-            if(newFilm.value.length > 21) {
-                newFilm.value = newFilm.value.slice(0,21) + '...';
+        let newFilm = addInput.value,
+            favourite = checkbox.checked;
+
+        if (newFilm) {
+            if(newFilm.length > 21) {
+                newFilm = `${newFilm.slice(0,21)}...`;
             }
-            movieDB.movies.push(newFilm.value.toUpperCase());
-            console.log(movieDB.movies);
-            newFilm.value = '';
-            myMovieContainer.innerHTML += (`<li class="promo__interactive-item">${movieDB.movies.length} 
-            ${movieDB.movies[movieDB.movies.length - 1]} <div class = 'delete'></div></li>`);
+
+            movieDB.movies.push(newFilm);
+            sortArrOfStr(movieDB.movies);
+            addSortedMovies(movieDB.movies, myMovieContainer);
+
+            if (favourite) {
+                console.log('Добавляем любимый фильм');
+            }
+
+            // return empty input field
+            addInput.value = '';
         }
     });
+
 });
