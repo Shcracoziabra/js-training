afdc462c7b60e10cd3419b782894aebfa1bc656b
Tetiana <brezhynska9012@gmail.com>
2021-02-21 16:22:01 +0200

    added projects from Lecturs up to 33

---
+++ preview/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
+
+
+/* btn.onclick = function() {
+    alert('Click');
+}; */
+
+/* btn.addEventListener('click', () => {
+    alert('Click');
+});
+
+btn.addEventListener('click', () => {
+    alert('Second Click');
+}); */
+
+let i=0;
+
+const deleteElement = (e) => {
+    console.log(e.currentTarget);
+    console.log(e.type);
+
+    /* i++;
+    if(i == 1){
+        btn.removeEventListener('click', deleteElement);
+    } */
+
+};
+
+/* btn.addEventListener('click', deleteElement);
+overlay.addEventListener('click', deleteElement); */
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log(event.target);
+});
+
+btns.forEach(btn => {
+   btn.addEventListener('click', deleteElement, {once: true});
+});
